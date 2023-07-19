b4a3f8bca41bacb8d15f8e4fffc6f2042711f16c
= <zuna150@gmail.com>
2023-07-19 14:42:03 +0200

    After lesson 40

--- cypress/e2e/contact-us.js
+++ cypress/e2e/contact-us.js
@@ -2,14 +2,22 @@
 
 describe("Test 'Contact Us' form via WebdriverUni", () => {
     //positive scenario
-    it.only("Should be able to submit a succesful submission via 'contact us' form", () => {
+    it("Should be able to submit a succesful submission via 'contact us' form", () => {
         cy.visit("http://www.webdriveruniversity.com/Contact-Us/contactus.html")
         //cy.get('#contact-us').click({force: true})
         cy.get('[name="first_name"]').type("Joe")
+        cy.get('[name="last_name"]').type("McDonald")
+        cy.get('[name="email"]').type("j.mcdonald@example.com")
+        cy.get('[name="message"]').type("lorem ipsum")
+        cy.get('[type="submit"]').click()
     });
 
     //negative scenario
-    it("Should not be able to submit a succesful submission via 'contact us' form as all fields are required", () => {
-        //cypress code
+    it.only("Should not be able to submit a succesful submission via 'contact us' form as all fields are required", () => {
+        cy.visit("http://www.webdriveruniversity.com/Contact-Us/contactus.html")
+        cy.get('[name="first_name"]').type("Mary")
+        cy.get('[name="last_name"]').type("Blowfish")
+        cy.get('[name="message"]').type("lorem ipsum")
+        cy.get('[type="submit"]').click()
     });
 })
